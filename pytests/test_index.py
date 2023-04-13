8d531151d8b378ee4b4b5e98bafa0f10ac96e415
Christine Egan <christine.egan@flatironschool.com>
2023-04-13 13:21:43 -0400

    old edits

--- pytests/test_index.py
+++ pytests/test_index.py
@@ -4,10 +4,20 @@ import pytest
 import importlib
 
 # importing objects from the jupyter notebook here
+<<<<<<< HEAD
 from ipynb.fs.full.index import * # variable names go here
 
 # format for writing tests
 # all functions that are to be run by test suite *must* be prepended with test_
+=======
+from ipynb.fs.full.index import # variable names go here
+
+# format for writing tests
+# all functions that are to be run by test suite *must* be prepended with test_
+def test_name_of_test_here():
+    assert True, "AssertionError will *not* raise and this message will not show"
+    assert False, "AssertionError will raise and output this message in the trace"
+>>>>>>> 1b234bb (Add files via upload)
 
 # tests to ensure correct environment is loaded
 def test_conda_environment_activated():
